fecf6b743c96089cad83fd06d9bcbf0149fc7c65
Aavez0430 <Aavezshaikh0430@gmail.com>
2023-07-15 21:19:55 +0530

    Add React Practice Project

---
+++ reactsession/practice/src/App.js
@@ -0,0 +1,12 @@
+
+import Counter from './compunent/Counter'
+function App() {
+  return (
+    <div className="App">
+      
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactsession/practice/src/compunent/Counter.js
@@ -0,0 +1,43 @@
+import React from "react";
+import '../style/Counter.css';
+
+class Counter extends React.Component{
+    constructor(props){
+        super();
+
+
+        this.state={
+            
+            count:0
+
+        
+        }
+        
+        //this.handleClick = this.handleClick.bind(this);
+    }
+
+
+handleClick=()=>{
+    //console.log('clicked.');
+    const count = this.state.count
+    this.setState({count:count+1});
+    // console.log("hello",count+1);
+    // if(count<=10){
+    //     console.log(this.handleClick);
+    // }
+}
+
+    render(){
+        return(
+            <div className="counter">
+                <h2>Counter</h2>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>click</button>
+            </div>
+
+        )
+    }
+
+}
+
+export default Counter;
